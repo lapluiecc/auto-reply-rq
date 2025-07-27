07a066afa5b386c6a5e2cea5a327a8d87aa7b546
lapluiecc <lapluiecc@gmail.com>
2025-07-28 00:01:29 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ from rq import Worker, Queue
 from rq.serializers import JSONSerializer
 from logger import log
 
-REDIS_URL = os.getenv("REDIS_URL", "rediss://default:...")
+REDIS_URL = os.getenv("REDIS_URL", "rediss://default:AV93AAIjcDFiMmYxMTY4MjI4NzE0MTVhOWRhZDY1YTk2YTVkMjlmNHAxMA@flexible-eft-24439.upstash.io:6379")
 redis_conn = Redis.from_url(REDIS_URL, decode_responses=True)
 queue = Queue(connection=redis_conn, serializer=JSONSerializer)
 
