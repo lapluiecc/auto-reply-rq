95aca19555b549d4f45d59df315e115ecdad85b8
lapluiecc <lapluiecc@gmail.com>
2025-07-29 19:33:21 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,10 +1,14 @@
+import os
 from redis import Redis
 from rq import Worker, Queue
 from rq.serializers import JSONSerializer
 from tasks import process_message
 from logger import log
 
-redis_conn = Redis(decode_responses=True)
+# Connexion Redis via URL de l’environnement
+REDIS_URL = os.getenv("REDIS_URL")
+redis_conn = Redis.from_url(REDIS_URL, decode_responses=True)
+
 queue = Queue(connection=redis_conn, serializer=JSONSerializer)
 
 if __name__ == "__main__":
