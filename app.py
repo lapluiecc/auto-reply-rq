747c98c8b31c66f6b2bb1a7386f724793aada9eb
lapluiecc <lapluiecc@gmail.com>
2025-07-29 20:18:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,9 @@ LOG_FILE = "/tmp/log.txt"
 
 app = Flask(__name__)
 
-# ✅ Connexion Redis sécurisée automatique si rediss://
+# ✅ Connexion Redis (automatique selon rediss:// ou redis://)
 REDIS_URL = os.getenv("REDIS_URL")
-redis_conn = Redis.from_url(
-    REDIS_URL,
-    decode_responses=True,
-    ssl=True if REDIS_URL and REDIS_URL.startswith("rediss://") else False
-)
+redis_conn = Redis.from_url(REDIS_URL, decode_responses=True)
 
 # ✅ File "default" bien déclarée
 q = Queue("default", connection=redis_conn, serializer=JSONSerializer)
