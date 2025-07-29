ddbd4f0d641ba0430d6eea3ddf619529c6a05fab
lapluiecc <lapluiecc@gmail.com>
2025-07-29 19:05:31 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,12 +1,8 @@
-import os
 from datetime import datetime, timezone
 
 LOG_FILE = "/tmp/log.txt"
 
 def log(text):
     print(text)
-    try:
-        with open(LOG_FILE, 'a', encoding='utf-8') as f:
-            f.write(f"[{datetime.now(timezone.utc).isoformat()}] {text}\n")
-    except Exception as e:
-        print(f"[LOGGER ERROR] {e}")
+    with open(LOG_FILE, 'a', encoding='utf-8') as f:
+        f.write(f"[{datetime.now(timezone.utc).isoformat()}] {text}\n")
